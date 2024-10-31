ea44cd87c965087a3d2d42dc1bcc6a28e38d8239
kh4n9 <mkhoangvip@gmail.com>
2024-10-31 20:31:38 +0700

    Add About, Contact, and Events pages; update routing and navigation items

--- client/src/components/InfoComponent/index.jsx
+++ client/src/components/InfoComponent/index.jsx
@@ -1,22 +1,29 @@
-import { Col, Row, Typography } from 'antd';
-const { Text } = Typography;
+import { Col, Image, Row, Typography, Input, Flex } from 'antd';
+
+const { Link, Title, Text } = Typography;
 
 const InfoComponent = (prop) => {
     return (
         <div>
-            <div>
-                <Row>
-                    <Col span={12}><Text>Họ tên: {prop.fullname}</Text></Col>
-                    <Col span={12}><Text>Mã số sinh viên: {prop.mssv}</Text></Col>
-                </Row>
-                <Row>
-                    <Col span={12}><Text>Khoa: {prop.department}</Text></Col>
-                    <Col span={12}><Text>Lớp: {prop.class}</Text></Col>
-                </Row>
-                <Row>
-                    <Col span={24}><Text>Niên khóa: {prop.schoolYear}</Text></Col>
-                </Row>
-            </div>
+            <Flex>
+                <Col span={6}>
+                    <Row><Image width={200} src={prop.avatarUrl} /></Row>
+                    <Row style={{marginTop:20}}><Link href="#">Change Avatar</Link></Row>
+                    <Row style={{marginTop:20}}><Link href="#">Change Password</Link></Row>
+                    <Row style={{marginTop:20}}><Link href="#">Edit Profile</Link></Row>
+                </Col>
+                <Col span={18}>
+                    <Row><Title style={{marginTop: 0}}>Thông tin tài khoản</Title></Row>
+                    <Row><Text>Họ tên</Text></Row>
+                    <Row><Input value={prop.fullname} /></Row>
+                    <Row style={{marginTop:20}}><Text>Tài khoản</Text></Row>
+                    <Row><Input value={prop.username}></Input></Row>
+                    <Row style={{marginTop:20}}><Text>Email</Text></Row>
+                    <Row><Input value={prop.email}></Input></Row>
+                    <Row style={{marginTop:20}}><Text>Số điện thoại</Text></Row>
+                    <Row><Input value={prop.phonenumber}></Input></Row>
+                </Col>
+            </Flex>
         </div>
     );
 }
--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -1,8 +1,11 @@
 import HomePage from '../pages/HomePage';
-import DSSVPage from '../pages/DSSVPage';
+import DSSVPage from '../pages/SinhvienPage';
 import NotFoundPage from '../pages/NotFoundPage';
 import LoginPage from '../pages/LoginPage';
 import UserProfilePage from '../pages/UserProfilePage';
+import EventsPage from '../pages/EventsPage';
+import AboutPage from '../pages/AboutPage';
+import ContactPage from '../pages/ContactPage';
 
 const routes = [
     {
@@ -11,8 +14,13 @@ const routes = [
         isHeader: true
     },
     {
-        path: '/dssv',
-        element: DSSVPage,
+        path: '/about',
+        element: AboutPage,
+        isHeader: true
+    },
+    {
+        path: '/contact',
+        element: ContactPage,
         isHeader: true
     },
     {
@@ -24,6 +32,26 @@ const routes = [
         path: '/login',
         element: LoginPage,
         isHeader: false
+    },
+    {
+        path: '/events',
+        element: EventsPage,
+        isHeader: true
+    },
+    {
+        path: '/sinhvien',
+        element: DSSVPage,
+        isHeader: true
+    },
+    {
+        path: '/bancansu',
+        element: DSSVPage,
+        isHeader: true
+    },
+    {
+        path: '/chungnhan',
+        element: DSSVPage,
+        isHeader: true
     },
     {
         path: '*',
